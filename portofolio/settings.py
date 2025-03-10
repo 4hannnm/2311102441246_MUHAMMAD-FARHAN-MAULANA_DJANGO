59251e64592c94057f3046eb387f6b1c85808359
farhan <2311102441246@umkt.ac.id>
2025-03-10 19:32:56 +0800

    update allowed_host in settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xyr&6%e+7kafhk*(0#8j-8r(82bxl+f$8e1hcc1=g#t4^=cf%p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
