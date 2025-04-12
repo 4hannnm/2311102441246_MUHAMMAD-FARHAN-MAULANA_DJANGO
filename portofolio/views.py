cb24451b5f0e4c6370d8a0ffb132b7d93b1242c4
Sigit-git <144525103+4hannm@users.noreply.github.com>
2025-04-12 23:43:13 +0800

    p5: update home.html menjadi halaman/index.html

--- portofolio/views.py
+++ portofolio/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
-    template_name = "home.html"
+    template_name = "halaman/index.html"
     context = {
         'title' : 'my home',
         'welcome' :'welcome my home',
     }
     return render(request, template_name, context)
+
+
 
 
 def about(request):
