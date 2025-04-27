6b8328791f57c356b4b1288460a7ce21acb39e35
farhan <2311102441246@umkt.ac.id>
2025-04-27 19:09:47 +0800

    P7: add forms.py and modified code

--- berita/views.py
+++ berita/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from berita.models import Kategori, Artikel
+from berita.forms import ArtikelForm
 
 # Create your views here.
 
@@ -60,3 +61,67 @@ def kategori_delete(request, id_kategori):
     except:
         pass
     return redirect(kategori_list)
+
+# Artikel
+
+def artikel_list(request) : 
+    template_name = "dashboard/snippets/artikel_list.html"
+    artikel = Artikel.objects.all()
+    print(artikel)
+    context = {
+        'title' : 'daftarartikel',
+        'artikel' : artikel
+    }
+    return render(request, template_name, context)
+
+def artikel_add(request):
+    template_name = "dashboard/snippets/artikel_forms.html"
+    if request.method == "POST":
+        forms = ArtikelForm(request.POST, request.FILES)
+        if forms.is_valid():
+            pub = forms.save(commit=False)
+            pub.author = request.user
+            pub.save()
+            return redirect(artikel_list)
+        else:
+            print(forms.errors)
+            
+    forms = ArtikelForm()
+    context = {
+        'title' : 'tambah artikel',
+        'forms' : forms
+    }
+    return render(request, template_name, context)
+
+def artikel_detail(request, id_artikel):
+    template_name = "dashboard/snippets/artikel_detail.html"
+    artikel = Artikel.objects.get(id= id_artikel)
+    context = {
+        'title' : artikel.judul,
+        'artikel' : artikel
+    }
+    return render(request, template_name, context)
+
+def artikel_update(request, id_artikel):
+    template_name = "dashboard/snippets/artikel_forms.html"
+    artikel = Artikel.objects.get(id=id_artikel)
+    if request.method == "POST":
+        forms = ArtikelForm(request.POST, request.FILES, instance=artikel)
+        if forms.is_valid():
+            pub = forms.save(commit=False)
+            pub.author = request.user
+            pub.save()
+            return redirect(artikel_list)
+            
+    forms = ArtikelForm(instance=artikel)
+    context = {
+        'title' : 'tambah kategori',
+        'forms' : forms
+    }
+    return render(request, template_name, context)
+
+def artikel_delete(request, id_artikel):
+    try:
+        Artikel.objects.get(id=id_artikel).delete()
+    except:pass
+    return redirect(artikel_list)
