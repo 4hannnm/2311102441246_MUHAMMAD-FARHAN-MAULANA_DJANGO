b0e89419fad6d27ead8f276d587be8125eedc472
hp <2311102441246@umkt.ac.id>
2025-05-21 22:52:33 +0800

    P10: Membuat User login dan registrasi

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import home, about, contact, detail_artikel
+from portofolio.views import home, about, contact, detail_artikel
+from portofolio.authentication import sign_in, sign_up, user_logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +14,11 @@ urlpatterns = [
     path('about', about, name='about'),
     path('contact', contact, name='contact'),
     path('detail/<slug:slug>', detail_artikel, name="detail_artikel"),
-    path('dashboard/', include("berita.urls"))
+    path('dashboard/', include("berita.urls")),
+    
+    path('authentication/sign_in', sign_in, name='sign_in'),
+    path('authentication/signup', sign_up, name='sign_up'),
+    path('authentication/login', user_logout, name='user_logout'),
 ]
 
 if settings.DEBUG: 
